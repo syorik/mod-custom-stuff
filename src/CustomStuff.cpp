d9da24753441313a3a70b438d41ae7b43a5bff33
Jaroslaw Sitarczuk <jaroslaw.sitarczuk@gmail.com>
2025-09-14 22:34:16 +0200

    feat: add goto command

--- src/CustomStuff.cpp
+++ src/CustomStuff.cpp
@@ -2,6 +2,8 @@
 #include "Player.h"
 #include "Config.h"
 #include "Chat.h"
+#include "World.h"
+#include "WorldSession.h"
 
 class CustomStuff : public PlayerScript
 {
@@ -61,8 +63,99 @@ private:
     }
 };
 
+class CustomStuffCommand : public CommandScript
+{
+public:
+    CustomStuffCommand() : CommandScript("CustomStuffCommand") {}
+
+    ChatCommandTable GetCommands() const override
+    {
+        static ChatCommandTable moduleCommandTable =
+        {
+            { "goto", HandleGotoCommand, SEC_PLAYER, Console::No }
+        };
+
+        static ChatCommandTable commandTable =
+        {
+            { "custom", moduleCommandTable },
+        };
+
+        return commandTable;
+    }
+
+    static bool HandleGotoCommand(ChatHandler* handler, std::string args)
+    {
+        Player* player = handler->GetSession()->GetPlayer();
+
+        if (sConfigMgr->GetOption<bool>("CustomStuff.Enable", false) == false)
+        {
+            handler->SendSysMessage("CustomStuff module is disabled");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (args.empty())
+        {
+            handler->SendSysMessage("Usage: .custom goto <player_name>");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // Check if player is in combat
+        if (player->IsInCombat())
+        {
+            handler->SendSysMessage("You cannot teleport while in combat!");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // Check if player is in flight
+        if (player->IsInFlight())
+        {
+            handler->SendSysMessage("You cannot teleport while flying!");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // Find target player
+        Player* targetPlayer = ObjectAccessor::FindPlayerByName(args);
+        if (!targetPlayer)
+        {
+            handler->SendSysMessage("Player not found or not online!");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // Don't allow teleporting to yourself
+        if (targetPlayer == player)
+        {
+            handler->SendSysMessage("You cannot teleport to yourself!");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // Get target player's position
+        float x = targetPlayer->GetPositionX();
+        float y = targetPlayer->GetPositionY();
+        float z = targetPlayer->GetPositionZ();
+        float o = targetPlayer->GetOrientation();
+        uint32 mapId = targetPlayer->GetMapId();
+
+        // Teleport the player
+        player->TeleportTo(mapId, x, y, z, o);
+
+        // Send confirmation message
+        std::ostringstream ss;
+        ss << "Teleported to " << targetPlayer->GetName() << "!";
+        handler->SendSysMessage(ss.str().c_str());
+
+        return true;
+    }
+};
+
 // Register your script(s) here
 void AddCustomStuffScripts()
 {
     new CustomStuff();
+    new CustomStuffCommand();
 }
