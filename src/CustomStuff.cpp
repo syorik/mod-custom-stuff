3c78f6a26d3a92d8db4879606e2ae88968648d69
Jaroslaw Sitarczuk <jaroslaw.sitarczuk@gmail.com>
2025-09-14 20:40:22 +0200

    feat: give starting bonuses on first login

--- src/CustomStuff.cpp
+++ src/CustomStuff.cpp
@@ -13,12 +13,27 @@ public:
             RestorePlayerResources(player);
     }
 
+    void OnPlayerFirstLogin(Player* player) override
+    {
+        if (sConfigMgr->GetOption<bool>("CustomStuff.Enable", false))
+            RestorePlayerResources(player);
+    }
+
 private:
+    const uint32 FROSTWEAVE_BAG = 41599;
+
     void RestorePlayerResources(Player* player)
     {
         player->SetHealth(player->GetMaxHealth());
         Powers p = player->getPowerType();
         player->SetPower(p, player->GetMaxPower(p));
+    }
+
+    void ApplyStartingBonuses(Player* player)
+    {
+        player->ModifyMoney(5000 * GOLD);
+        player->GiveLevel(20);
+        player->AddItem(FROSTWEAVE_BAG, 4);
     }
 };
 
