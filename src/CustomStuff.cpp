4389db0b6b8b4926894ef7a964d5e0436c4e2a5a
Jaroslaw Sitarczuk <jaroslaw.sitarczuk@gmail.com>
2025-09-14 21:35:33 +0200

    feat: add congrats on level

--- src/CustomStuff.cpp
+++ src/CustomStuff.cpp
@@ -1,6 +1,7 @@
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Config.h"
+#include "Chat.h"
 
 class CustomStuff : public PlayerScript
 {
@@ -19,6 +20,12 @@ public:
             ApplyStartingBonuses(player);
     }
 
+    void OnPlayerLevelChanged(Player* player, uint8 oldLevel) override
+    {
+        if (sConfigMgr->GetOption<bool>("CustomStuff.Enable", false))
+            SendCongratulationsMessage(player);
+    }
+
 private:
     const uint32 FROSTWEAVE_BAG = 41599;
 
@@ -34,6 +41,11 @@ private:
         player->ModifyMoney(sConfigMgr->GetOption<uint32>("CustomStuff.StartingGold", 500) * GOLD);
         player->GiveLevel(sConfigMgr->GetOption<uint32>("CustomStuff.StartingLevel", 15));
         player->AddItem(FROSTWEAVE_BAG, 4);
+    }
+
+    void SendCongratulationsMessage(Player* player)
+    {
+        ChatHandler(player->GetSession()).PSendSysMessage("GZ %s!", player->GetName().c_str());
     }
 };
 
